a57ba8392874350286580f3d31efd5cb628bf54a
unknown <bond.zonda777@gmail.com>
2020-06-26 16:06:16 +0300

    Доработана мобильная версия приложения

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger,.header__nav').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('.header__list').click(function(event) {
+	   $('.header__burger,.header__nav').removeClass('active');
+	   $('body').removeClass('lock');
+	});
+});
